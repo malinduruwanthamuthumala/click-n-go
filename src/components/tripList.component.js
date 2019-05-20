2dc587042b93ccf956f665ace6209a54d7fe29b6
unknown <malinduruwantha@gmail.com>
2019-05-20 09:06:55 +0530

    trip list component

---
+++ src/components/tripList.component.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+export default class tripList extends Component{
+render(){
+    return(
+        <div>
+            <h1>Welcome to trip listcomponent</h1>
+        </div>
+    )
+}
+}
