49cff7b5999216cd50fa7d37414b908c29c2c064
unknown <malinduruwantha@gmail.com>
2019-05-20 09:06:45 +0530

    edit trip component

---
+++ src/components/edittripList.component.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+export default class edittripList extends Component{
+render(){
+    return(
+        <div>
+            <h1>Welcome to edit trip component</h1>
+        </div>
+    )
+}
+}
