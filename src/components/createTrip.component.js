5938a54d1162342177ef571611cb03cc8027948c
unknown <malinduruwantha@gmail.com>
2019-05-21 20:42:36 +0530

    bind methods to this keyword

--- src/components/createTrip.component.js
+++ src/components/createTrip.component.js
@@ -5,6 +5,13 @@ export default class createTrip extends Component{
 
 constructor(props){
     super(props);
+
+    this.onChangeDescription=this.onChangeDescription.bind(this);
+    this.onChangePassengers=this.onChangePassengers.bind(this);
+    this.onChangeagencyName=this.onChangeagencyName.bind(this);
+    this.onChangetelephone=this.onChangetelephone.bind(this);
+    this.onChangetitle=this.onChangetitle.bind(this);
+
     this.state={
         description:'',
         title:'',
@@ -51,7 +58,21 @@ onChangeagencyName(data){
         agency_name:data.target.value
     })
 }
+onSubmit(data){
+    data.preventDefault();
 
+    console.log('form submitted');
+    console.log('description:${this.state.description}');
+    console.log('description:${this.state.title}');
+    this.setState({
+        description:'',
+        title:'',
+        telephone:'',
+        passengers:'',
+        location:'',
+        agency_name:'',
+    })
+}
 render(){
     return(
         <div>
